946c852d420a8ceee80d80b2d15c615b8a16fd3e
David Barnes <dbarnes@kvcc.edu>
2017-08-27 16:13:58 -0400

    Initial commit

---
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
@@ -0,0 +1,13 @@
+package edu.kvcc.cis298.cis298assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CounterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+    }
+}
